bce24cf297de44295a3abe77ff5d9796dad640ba
ShadowWolf <league@WTS15.attlocal.net>
2017-11-26 16:00:11 -0800

    Completed

--- src/thread_pool/ThreadPool.java
+++ src/thread_pool/ThreadPool.java
@@ -1,5 +1,32 @@
 package thread_pool;
 
+import java.util.ArrayDeque;
+
 public class ThreadPool {
-	
+	private int totalThreads;
+	private ArrayDeque<Task> taskQueve;
+	private Thread[] threads;
+	public ThreadPool(int totalThreads) {
+		this.totalThreads = totalThreads;
+		taskQueve = new ArrayDeque<Task>();
+		threads = new Thread[totalThreads];
+		for (int i = 0; i < totalThreads; i++) {
+			threads[i] = new Thread(new Worker(taskQueve));
+		}
+	}
+	public void addTask(Task task) {
+		taskQueve.add(task);
+	}
+	public void start() {
+		for (int i = 0; i < threads.length; i++) {
+			threads[i].start();
+		}
+		for (int j = 0; j < threads.length; j++) {
+			try {
+				threads[j].join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}			
+		}
+	}
 }
--- src/thread_pool/Worker.java
+++ src/thread_pool/Worker.java
@@ -1,12 +1,18 @@
 package thread_pool;
 
+import java.util.ArrayDeque;
 
 public class Worker implements Runnable {
-
-	@Override
+	private ArrayDeque<Task> taskQueve;
+	public Worker(ArrayDeque<Task> taskQueve) {
+		this.taskQueve = taskQueve;
+		taskQueve = new ArrayDeque<Task>();
+	}
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		while (taskQueve.size() >= 1) {
+			Task t = taskQueve.remove();
+			t.perform();
+		}
 	}
 
 
